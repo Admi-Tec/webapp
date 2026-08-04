9746ab19ae40f043c0aadb5319f5b5c6ddcb2f5f
Luis ENRIQUEZ TORRES <lenriquez389@gmail.com>
2026-08-04 21:45:59 +0200

    KAN-8 add favicon.ico for google

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -116,6 +116,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32.png" },
       { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16.png" },
+      { rel: "icon", sizes: "any", href: "/favicon.ico" },
       { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
